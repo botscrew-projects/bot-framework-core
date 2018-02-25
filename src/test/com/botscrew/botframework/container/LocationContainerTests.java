853d7b0d65b469394754d8b74b54fe47184644b5
vlad <mailaka@ukr.net>
2018-02-25 13:01:44 +0200

    add tests for LocationContainer

---
+++ src/test/com/botscrew/botframework/container/LocationContainerTests.java
@@ -0,0 +1,142 @@
+package com.botscrew.botframework.container;
+
+import com.botscrew.botframework.container.domain.LocationHandlerImpl;
+import com.botscrew.botframework.container.domain.TextHandlerImpl;
+import com.botscrew.botframework.container.domain.UserImpl;
+import com.botscrew.botframework.model.ChatUser;
+import com.botscrew.botframework.model.GeoCoordinates;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+public class LocationContainerTests {
+
+    @Test
+    public void locationShouldBeAvailableIfPassed() {
+        GeoCoordinates location = new GeoCoordinates(1.0, 1.0);
+        UserImpl user = new UserImpl("default");
+
+        List<Object> arguments = executeLocation(location, user);
+
+        assert arguments.get(0).equals(location);
+    }
+
+    @Test
+    public void textContainerShouldPassChatUserImplToMethod() {
+        GeoCoordinates location = new GeoCoordinates(1.0, 1.0);
+        String state = "default";
+        UserImpl user = new UserImpl(state);
+
+        List<Object> arguments = executeLocation(location, user);
+
+        assert arguments.get(1).getClass().equals(UserImpl.class);
+        assert ((UserImpl) arguments.get(1)).getState().equals(state);
+    }
+
+    @Test
+    public void textContainerShouldPassStringParametersToTargetMethod() throws Exception {
+        String paramKey = "param";
+        String paramValue = "value";
+        String state = "stringParam?" + paramKey + "=" + paramValue;
+        GeoCoordinates location = new GeoCoordinates(1.0, 1.0);
+        UserImpl user = new UserImpl(state);
+
+        List<Object> arguments = executeLocation(location, user);
+
+        assert arguments.get(1).equals(paramValue);
+    }
+
+    @Test
+    public void textContainerShouldPassIntParametersToTargetMethod() throws Exception {
+        GeoCoordinates location = new GeoCoordinates(1.0, 1.0);
+        String paramKey = "param";
+        Integer paramValue = 1;
+        String state = "intParam?" + paramKey + "=" + paramValue;
+        UserImpl user = new UserImpl(state);
+
+        List<Object> arguments = executeLocation(location, user);
+
+        assert arguments.get(1).equals(paramValue);
+    }
+
+    @Test
+    public void textContainerShouldPassLongParametersToTargetMethod() throws Exception {
+        GeoCoordinates location = new GeoCoordinates(1.0, 1.0);
+        String paramKey = "param";
+        Long paramValue = 10L;
+        String state = "longParam?" + paramKey + "=" + paramValue;
+        UserImpl user = new UserImpl(state);
+
+        List<Object> arguments = executeLocation(location, user);
+
+        assert arguments.get(1).equals(paramValue);
+    }
+
+    @Test
+    public void textContainerShouldPassByteParametersToTargetMethod() throws Exception {
+        GeoCoordinates location = new GeoCoordinates(1.0, 1.0);
+        String paramKey = "param";
+        Byte paramValue = 5;
+        String state = "byteParam?" + paramKey + "=" + paramValue;
+        UserImpl user = new UserImpl(state);
+
+        List<Object> arguments = executeLocation(location, user);
+
+        assert arguments.get(1).equals(paramValue);
+    }
+
+    @Test
+    public void textContainerShouldPassShortParametersToTargetMethod() throws Exception {
+        GeoCoordinates location = new GeoCoordinates(1.0, 1.0);
+        String paramKey = "param";
+        Short paramValue = 3;
+        String state = "shortParam?" + paramKey + "=" + paramValue;
+        UserImpl user = new UserImpl(state);
+
+        List<Object> arguments = executeLocation(location, user);
+
+        assert arguments.get(1).equals(paramValue);
+    }
+
+    @Test
+    public void textContainerShouldPassFloatParametersToTargetMethod() throws Exception {
+        GeoCoordinates location = new GeoCoordinates(1.0, 1.0);
+        String paramKey = "param";
+        Float paramValue = 4.83f;
+        String state = "floatParam?" + paramKey + "=" + paramValue;
+        UserImpl user = new UserImpl(state);
+
+        List<Object> arguments = executeLocation(location, user);
+
+        assert arguments.get(1).equals(paramValue);
+    }
+
+    @Test
+    public void textContainerShouldPassDoubleParametersToTargetMethod() throws Exception {
+        GeoCoordinates location = new GeoCoordinates(1.0, 1.0);
+        String paramKey = "param";
+        Double paramValue = 224332.56;
+        String state = "doubleParam?" + paramKey + "=" + paramValue;
+        UserImpl user = new UserImpl(state);
+
+        List<Object> arguments = executeLocation(location, user);
+
+        assert arguments.get(1).equals(paramValue);
+    }
+
+    private List<Object> executeLocation(GeoCoordinates location, ChatUser user) {
+        LocationContainer textContainer = new LocationContainer();
+        LocationHandlerImpl handler = new LocationHandlerImpl();
+        List<Object> arguments = new ArrayList<>();
+        handler.addFollower(args -> arguments.addAll(Arrays.asList(args)));
+        textContainer.register(handler);
+
+        textContainer.processLocation(location, user);
+        return arguments;
+    }
+}
---
+++ src/test/com/botscrew/botframework/container/domain/LocationHandlerImpl.java
@@ -0,0 +1,82 @@
+package com.botscrew.botframework.container.domain;
+
+import com.botscrew.botframework.annotation.Location;
+import com.botscrew.botframework.annotation.Param;
+import com.botscrew.botframework.annotation.Text;
+import com.botscrew.botframework.model.GeoCoordinates;
+
+public class LocationHandlerImpl extends Followable {
+
+    @Location
+    public void consumeTextAndUser(GeoCoordinates coordinates, UserImpl user) {
+        Object[] args = new Object[2];
+        args[0] = coordinates;
+        args[1] = user;
+
+        getFollowers().forEach(c -> c.accept(args));
+    }
+
+    @Location(states = {"stringParam"})
+    public void consumeTextAndStringParam(UserImpl user, @Param(name = "param") String param) {
+        Object[] args = new Object[2];
+        args[0] = user;
+        args[1] = param;
+
+        getFollowers().forEach(c -> c.accept(args));
+    }
+
+    @Location(states = {"intParam"})
+    public void consumeTextAndIntParam(UserImpl user, @Param(name = "param") Integer param) {
+        Object[] args = new Object[2];
+        args[0] = user;
+        args[1] = param;
+
+        getFollowers().forEach(c -> c.accept(args));
+    }
+
+    @Location(states = {"longParam"})
+    public void consumeTextAndLongParam(UserImpl user, @Param(name = "param") Long param) {
+        Object[] args = new Object[2];
+        args[0] = user;
+        args[1] = param;
+
+        getFollowers().forEach(c -> c.accept(args));
+    }
+
+    @Location(states = {"byteParam"})
+    public void consumeTextAndByteParam(UserImpl user, @Param(name = "param") Byte param) {
+        Object[] args = new Object[2];
+        args[0] = user;
+        args[1] = param;
+
+        getFollowers().forEach(c -> c.accept(args));
+    }
+
+    @Location(states = {"shortParam"})
+    public void consumeTextAndIntParam(UserImpl user, @Param(name = "param") Short param) {
+        Object[] args = new Object[2];
+        args[0] = user;
+        args[1] = param;
+
+        getFollowers().forEach(c -> c.accept(args));
+    }
+
+
+    @Location(states = {"floatParam"})
+    public void consumeTextAndIntParam(UserImpl user, @Param(name = "param") Float param) {
+        Object[] args = new Object[2];
+        args[0] = user;
+        args[1] = param;
+
+        getFollowers().forEach(c -> c.accept(args));
+    }
+
+    @Location(states = {"doubleParam"})
+    public void consumeTextAndIntParam(UserImpl user, @Param(name = "param") Double param) {
+        Object[] args = new Object[2];
+        args[0] = user;
+        args[1] = param;
+
+        getFollowers().forEach(c -> c.accept(args));
+    }
+}
