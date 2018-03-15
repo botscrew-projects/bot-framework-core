7ac31bef810f5c8dc74e11e7bc81297d4b395723
VladHanzha <mailaka@ukr.net>
2018-03-15 18:52:04 +0200

    fix IntentProcessorAnnotationBPP

--- src/main/java/com/botscrew/botframework/configuration/bpp/IntentProcessorAnnotationBPP.java
+++ src/main/java/com/botscrew/botframework/configuration/bpp/IntentProcessorAnnotationBPP.java
@@ -4,21 +4,33 @@ import com.botscrew.botframework.annotation.IntentProcessor;
 import com.botscrew.botframework.container.IntentMethodGroup;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntentProcessorAnnotationBPP implements BeanPostProcessor {
     private IntentMethodGroup intentMethodGroup;
+    private List<Object> intentProcessors = new ArrayList<>();
 
     @Override
     public Object postProcessBeforeInitialization(Object o, String s) {
-        if (o instanceof IntentMethodGroup) {
-            intentMethodGroup = (IntentMethodGroup) o;
-        }
         return o;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object o, String s) {
+        if (intentMethodGroup != null) {
+            intentMethodGroup.register(o);
+            return o;
+        }
+
+        if (o instanceof IntentMethodGroup) {
+            intentMethodGroup = (IntentMethodGroup) o;
+            intentProcessors.forEach(p -> intentMethodGroup.register(p));
+            return o;
+        }
+
         if (o.getClass().isAnnotationPresent(IntentProcessor.class)) {
-           intentMethodGroup.register(o);
+            intentProcessors.add(o);
         }
         return o;
     }
