286f2eabb439f906ff5e6a6d552dd09b83d77969
Vlad Hanzha <vlad.hanzha@botscrew.com>
2018-08-01 10:51:53 +0300

    Add missed @ConditionalOnMissingBean

--- src/main/java/com/botscrew/botframework/configuration/BotFrameworkConfiguration.java
+++ src/main/java/com/botscrew/botframework/configuration/BotFrameworkConfiguration.java
@@ -21,6 +21,7 @@ import com.botscrew.botframework.configuration.bpp.IntentProcessorAnnotationBPP;
 import com.botscrew.botframework.container.*;
 import com.botscrew.botframework.domain.method.group.impl.*;
 import com.botscrew.botframework.sender.PlatformSender;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -41,86 +42,103 @@ public class BotFrameworkConfiguration {
     }
 
     @Bean
+    @ConditionalOnMissingBean(PostbackHandlingMethodGroup.class)
     public PostbackHandlingMethodGroup postbackHandlingMethodGroup() {
         return new PostbackHandlingMethodGroup();
     }
 
     @Bean
+    @ConditionalOnMissingBean(PostbackContainer.class)
     public PostbackContainer postbackContainer(PostbackHandlingMethodGroup postbackHandlingMethodGroup) {
         return new PostbackContainer(postbackHandlingMethodGroup);
     }
 
     @Bean
+    @ConditionalOnMissingBean(TextHandlingMethodGroup.class)
     public TextHandlingMethodGroup textHandlingMethodGroup() {
         return new TextHandlingMethodGroup();
     }
 
     @Bean
+    @ConditionalOnMissingBean(TextContainer.class)
     public TextContainer textContainer(TextHandlingMethodGroup methodGroup) {
         return new TextContainer(methodGroup);
     }
 
     @Bean
+    @ConditionalOnMissingBean(ReadHandlingMethodGroup.class)
     public ReadHandlingMethodGroup readHandlingMethodGroup() {
         return new ReadHandlingMethodGroup();
     }
 
     @Bean
+    @ConditionalOnMissingBean(ReadContainer.class)
     public ReadContainer readContainer(ReadHandlingMethodGroup methodGroup) {
         return new ReadContainer(methodGroup);
     }
 
     @Bean
+    @ConditionalOnMissingBean(EchoHandlingMethodGroup.class)
     public EchoHandlingMethodGroup echoHandlingMethodGroup() {
         return new EchoHandlingMethodGroup();
     }
 
     @Bean
+    @ConditionalOnMissingBean(EchoContainer.class)
     public EchoContainer echoContainer(EchoHandlingMethodGroup methodGroup) {
         return new EchoContainer(methodGroup);
     }
 
     @Bean
+    @ConditionalOnMissingBean(DeliveryHandlingMethodGroup.class)
     public DeliveryHandlingMethodGroup deliveryHandlingMethodGroup() {
         return new DeliveryHandlingMethodGroup();
     }
 
     @Bean
+    @ConditionalOnMissingBean(DeliveryContainer.class)
     public DeliveryContainer deliveryContainer(DeliveryHandlingMethodGroup methodGroup) {
         return new DeliveryContainer(methodGroup);
     }
 
     @Bean
+    @ConditionalOnMissingBean(ReferralHandlingMethodGroup.class)
     public ReferralHandlingMethodGroup referralHandlingMethodGroup() {
         return new ReferralHandlingMethodGroup();
     }
 
     @Bean
+    @ConditionalOnMissingBean(ReferralContainer.class)
     public ReferralContainer referralContainer(ReferralHandlingMethodGroup methodGroup) {
         return new ReferralContainer(methodGroup);
     }
 
     @Bean
+    @ConditionalOnMissingBean(LocationHandlingMethodGroup.class)
     public LocationHandlingMethodGroup locationHandlingMethodGroup() {
         return new LocationHandlingMethodGroup();
     }
 
     @Bean
+    @ConditionalOnMissingBean(LocationContainer.class)
     public LocationContainer locationContainer(LocationHandlingMethodGroup locationHandlingMethodGroup) {
         return new LocationContainer(locationHandlingMethodGroup);
     }
 
     @Bean
+    @ConditionalOnMissingBean(IntentHandlingMethodGroup.class)
     public IntentHandlingMethodGroup intentMethodGroup() {
         return new IntentHandlingMethodGroup();
     }
 
     @Bean
+    @ConditionalOnMissingBean(IntentContainer.class)
     public IntentContainer intentContainer(IntentHandlingMethodGroup intentMethodGroup) {
         return new IntentContainer(intentMethodGroup);
     }
 
     @Bean
+    @ConditionalOnMissingBean(PlatformSender.class)
     public PlatformSender platformSender() {
         return new PlatformSender();
     }
