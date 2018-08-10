49070a406f68e558963c225d59680596c17c3416
Nikita Nikiforov <nikita.nikiforov@botscrew.com>
2018-08-10 13:07:12 +0300

    Add unified Coordinates model

---
+++ src/main/java/com/botscrew/botframework/domain/argument/element/Coordinates.java
@@ -0,0 +1,27 @@
+package com.botscrew.botframework.domain.argument.element;
+
+public class Coordinates {
+    private float longitude;
+    private float latitude;
+
+    public Coordinates(float longitude, float latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+}
