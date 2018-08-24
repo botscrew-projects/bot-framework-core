3cfee731b7e6a51c2a398dac7048511b50f9c15c
Oles Pankiv <oles.pankiv@botscrew.com>
2018-08-24 11:40:41 +0300

    Fixed PlatformSender

--- src/main/java/com/botscrew/botframework/sender/PlatformSender.java
+++ src/main/java/com/botscrew/botframework/sender/PlatformSender.java
@@ -21,13 +21,14 @@ import com.botscrew.botframework.domain.user.Platform;
 
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.concurrent.ScheduledFuture;
 
 /**
  * Base implementation for sending messages to different platforms based on `Bot` parameter
- *
+ * <p>
  * Available for registration platforms via `addSender` method.
  */
-public class PlatformSender implements Sender<Bot, Message> {
+public class PlatformSender {
     private final Map<Platform, Sender> platformSenders;
 
     public PlatformSender() {
@@ -36,15 +37,19 @@ public class PlatformSender implements Sender<Bot, Message> {
 
     /**
      * Register sender for specific platform
+     *
      * @param platform platform where this sender sends messages
-     * @param sender {@link Sender} implementation for specific platform
+     * @param sender   {@link Sender} implementation for specific platform
      */
     public void addSender(Platform platform, Sender sender) {
         platformSenders.put(platform, sender);
     }
 
-    @Override
     public void send(Bot bot, Message message) {
         platformSenders.get(bot.getPlatform()).send(bot, message);
+    }
+
+    public ScheduledFuture send(Bot bot, Message message, int delay) {
+        return platformSenders.get(bot.getPlatform()).send(bot, message, delay);
     }
 }
